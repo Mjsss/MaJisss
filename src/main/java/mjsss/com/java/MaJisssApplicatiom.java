debe84ff64b4ec5c0affb32eb1458136e69aeee5
馬記 <maji132011@gmail.com>
2019-08-17 20:40:22 +0900

    run application

--- src/main/java/mjsss/com/java/MaJisssApplicatiom.java
+++ src/main/java/mjsss/com/java/MaJisssApplicatiom.java
@@ -1,14 +1,23 @@
 package mjsss.com.java;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-import static org.springframework.boot.SpringApplication.run;
 
 @SpringBootApplication
-public class MaJisssApplicatiom {
-    public static void main(String[] args) {
-        run(MaJisssApplicatiom.class, args);
+public class MaJisssApplicatiom extends SpringBootServletInitializer{
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MaJisssApplicatiom.class);
+    }
+
+    public static void main(String[] args){
+        SpringApplication.run(MaJisssApplicatiom.class, args);
     }
 
 }
+
 
